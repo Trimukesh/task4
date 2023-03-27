310cad6add03a91a2d88365c45e262c25f4af974
srinivasareddy4218 <63068380+srinivasareddy4218@users.noreply.github.com>
2023-03-27 15:33:14 +0530

    Update manual_approval.ps1

--- manual_approval.ps1
+++ manual_approval.ps1
@@ -1,7 +1,23 @@
-$repoUrl = "https://github.com/Trimukesh/task4.git"
+$repoUrl = "https://github.com/your-username/your-repo"
 $approvalMsg = "Do you want to continue with the operation in $repoUrl? (Y/N)"
+$approverList = "user1@example.com,user2@example.com"
+
 $approval = Read-Host $approvalMsg
 if ($approval -eq "Y") {
+    # Send approval request to approver list
+    $approverArray = $approverList.Split(",")
+    foreach ($approver in $approverArray) {
+        # Send email or other notification to each approver
+        Write-Host "Sending approval request to $approver..."
+    }
+    # Wait for approval from all approvers
+    $approvalGranted = $false
+    while (-not $approvalGranted) {
+        # Check if all approvers have approved the request
+        # Set $approvalGranted to $true if all approvers have approved
+        Write-Host "Waiting for approval from all approvers..."
+        Start-Sleep -Seconds 10
+    }
     # Perform desired action(s) here
     Write-Host "Continuing with operation in $repoUrl..."
 } else {
